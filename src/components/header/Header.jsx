fcb6f06c129da5e807b6571a88d99f4ca6f368d1
yutabee <bsk87m@gmail.com>
2022-07-01 20:06:22 +0900

    task-card-components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div>
+            <header><h1>simple trello</h1></header>
+        </div>
+    );
+};
---
+++ src/components/task/TaskCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { TaskCardDeleteButton } from "./button/TaskCardDeleteButton";
+import { TaskAddInput } from "./input/TaskAddInput";
+import { TaskCardTitle } from "./TaskCardTitle";
+import { Tasks } from "./Tasks";
+
+export const TaskCard = () => {
+    return (
+    <div className="taskCard">
+            <TaskCardTitle />
+            <TaskCardDeleteButton />
+            <TaskAddInput />
+            <Tasks/>
+    </div>
+    );
+};
